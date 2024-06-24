e70619012bd1d7f0240922279094760950c7d5eb
acr6113 <acrchandan6113@gmail.com>
2024-06-25 00:00:48 +0530

    Created Chat App

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import { useState, useRef } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import io from 'socket.io-client';
+import Chat from './Chat';
+
+const socket = io('http://localhost:3001');
+
+
+function Join({ socket }) {
+  const [roomId, setRoomId] = useState('');
+  const [uname, setUname] = useState('');
+  const navigate = useNavigate();
+
+  function joinRoom() {
+  
+    if(roomId !== '' && uname !== ''){
+    sessionStorage.setItem("roomId", roomId)
+    sessionStorage.setItem("uname", uname)
+    socket.current.emit('joinRoom', {roomId, uname});
+      navigate(`/chat/${roomId}`);
+    }
+  }
+
+  return (
+    <div className='joinroom h-screen w-full flex flex-col gap-6 justify-center items-center bg-slate-800 text-xl'>
+      <form className='flex flex-col gap-6 justify-center items-center border p-6 rounded-xl'>
+        <input type='text' value={uname} className='p-4 rounded-xl' onChange={e => setUname(e.target.value)} placeholder='username' required/>      
+        <input type='text' value={roomId} className='p-4 rounded-xl' onChange={(e) => setRoomId(e.target.value)} placeholder='Enter  Room ID' required/>
+        <button type='submit' onClick={joinRoom} className='bg-slate-600 w-full p-4 rounded-xl hover:bg-lime-300 transition duration-500'>JOIN OR CREATE ROOM</button>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  const socketRef = useRef(socket);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Join socket={socketRef} />} />
+        <Route path='/chat/:id' element={<Chat socket={socketRef}/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require('http');
+const cors = require('cors')
+const app = express()
+const admin = require("firebase-admin");
+const serviceAccount = require("./api/chatapp-25360-firebase-adminsdk-27m7f-e517b4feaa.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chatapp-25360-default-rtdb.firebaseio.com"
+});
+const db = admin.database()
+
+app.use(cors())
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST']
+    }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+async function addUser(roomId, uname){
+    let ref = await db.ref("rooms").child(roomId).child("unames")
+    let newUnames = {
+        uname: uname,
+        time: new Date().toString()
+    }
+    ref.once('value').then((snapshot) => {
+            ref.child(snapshot.val().length).set(newUnames).then(a => console.log("inserted")).catch(a => console.log("unable"))
+    })
+}
+
+
+io.on('connection', (socket) => {
+        
+    socket.on('joinRoom', async ({roomId, uname})=>{
+        socket.join(roomId)
+        console.log('joined room: ', roomId, uname)
+        const room = db.ref('rooms').child(roomId)
+        await room.once('value', (snapshot) => {
+            if(snapshot.exists())
+                addUser(roomId, uname)
+            else{
+                room.set({
+                    unames: [{uname: uname, time: new Date().toString()}],
+                    datetime: new Date().toString()
+                });
+            }
+        })
+        socket.emit('roomId', {roomId, uname})
+        
+    })    
+
+    socket.on('send_message', async ({message, roomId}) => {
+        const messagesRef = await db.ref('rooms').child(roomId).child('messages')
+        await messagesRef.once('value').then(async (snapshot) => {
+            let messages = []
+            if(snapshot.exists())
+                messages = snapshot.val()
+            
+            messages.push(message)
+            
+            await messagesRef.set(messages)
+            console.log('message added')
+            return message
+        }).then((message) => {
+            console.log(message)
+            socket.in(roomId).emit('recieved_messages', message);
+        })
+    })
+})
+
+
+server.listen(3001, () => {
+    console.log('SERVER RUNNING ON 3001')
+})
+
