e6f489150a3231dae2065de0a58b219d31c48a46
acr6113 <acrchandan6113@gmail.com>
2024-07-01 23:22:10 +0530

    Updated JSON

--- server/index.js
+++ server/index.js
@@ -22,9 +22,9 @@ const io = new Server(server, {
     }
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html')
-// })
+app.get('/', (req, res) => {
+  res.json("Hello")
+})
 
 
 async function addUser(roomId, uname){
